e666cf552f930010b7a98bfc060ff59c1f84908d
Dhrubajyoti Bhattacharjee <dhrubajyotibhattacharjee182@gmail.com>
2025-02-04 00:57:43 +0530

    added the metadata.ts file for the sanity studio page

---
+++ app/studio/[[...tool]]/metadata.ts
@@ -0,0 +1,12 @@
+import { Metadata, Viewport } from "next";
+
+export const metadata: Metadata = {
+  title: "Sanity Studio",
+  description: "Sanity Content Management Studio",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+};
